a1bb97b218004d2e08ec01607b4e2b1754df3769
roxana.ghenea <roxana.ghenea@gmail.com>
2021-08-29 16:07:07 +0300

    test 1

---
+++ src/main/java/com/tutorial/repository/ThemeParkRideRepository.java
@@ -0,0 +1,10 @@
+package com.tutorial.repository;
+
+import com.tutorial.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
